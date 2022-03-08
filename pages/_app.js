53cfa597c9eebe5e9580623bceb6f95ed6ab6f95
Alex <alexcucos1@gmail.com>
2022-03-08 12:24:12 -0500

    Initial Commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,37 @@
+import { useEffect, useState } from 'react'
+import Head from 'next/head'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import { useRouter } from 'next/router'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [renderFooter, setRenderFooter] = useState(false)
+  const { pathname } = useRouter()
+
+  // useEffect(()=> {
+  //   const { pathname } = router  
+  //   if(pathname !== '/'){
+  //     setRenderFooter(true)
+  //   }
+  //   else {
+  //     setRenderFooter(false)
+  //   }
+  // }, [setRenderFooter])
+  
+  
+  return (
+    <>
+      <Head>
+        <title>Charisma</title>
+        <meta name="description" content="Wallet Personality Analysis" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Navbar/>
+      <Component {...pageProps} />
+      {pathname !== '/' ? <Footer /> : <></>}
+    </>
+  )
 }
 
 export default MyApp
