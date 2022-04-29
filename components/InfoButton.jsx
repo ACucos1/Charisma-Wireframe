1a78851b445718e507c799a6a5ac7911f53e7c0a
Tue Nguyen <tnguyen187@myseneca.ca>
2022-04-29 15:57:46 -0400

    Add onclick for info button in mobile

--- components/InfoButton.jsx
+++ components/InfoButton.jsx
@@ -1,14 +1,18 @@
 import { IconButton, Tooltip, Typography } from "@mui/material";
 import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Web3Context } from "../contexts/Web3Context";
 
 const InfoButton = () => {
   const { loggedIn } = useContext(Web3Context);
+  const [showTooltip, setShowTooltip] = useState(false);
   const renderInfoButton = () => {
     if (loggedIn) {
       return (
         <Tooltip
+          open={showTooltip}
+          onOpen={() => setShowTooltip(true)}
+          onClose={() => setShowTooltip(false)}
           placement="bottom"
           title={
             <Typography fontSize="1rem">
@@ -17,7 +21,7 @@ const InfoButton = () => {
           }
           arrow
         >
-          <IconButton>
+          <IconButton onClick={() => setShowTooltip(!showTooltip)}>
             <InfoOutlinedIcon sx={{ color: "#8ec3e6" }} />
           </IconButton>
         </Tooltip>
