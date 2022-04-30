c68fc5dc8b55ac95b5a3d384543ea5e2fe3e5877
Alex <alexcucos1@gmail.com>
2022-04-30 12:57:29 -0400

    result display no emojis on NGMI

--- components/ResultDisplay.jsx
+++ components/ResultDisplay.jsx
@@ -20,71 +20,75 @@ export default function ResultDisplay({ wpi }) {
             {wpi && wpi["4"].info.LongDesc}
           </li>
         </ul>
+        {wpi && wpi["1"].info.Value !== "N" && 
         <div className={styles.images}>
 
 
-          {wpi && wpi["1"].info.Value == "D" ? 
-            <img
-            src="./images/diamond.svg"
-            alt=""
-            className={`${styles.img} ${styles.diamond}`}
+        {wpi && wpi["1"].info.Value == "D" ? 
+          <img
+          src="./images/diamond.svg"
+          alt=""
+          className={`${styles.img} ${styles.diamond}`}
+        />
+        :
+        <img
+          src="./images/toiletpaper.svg"
+          alt=""
+          className={`${styles.img} ${styles.toiletpaper}`}
+        />
+
+        }
+
+        {wpi && wpi["2"].info.Value === "H" ? 
+          <img
+          src="./images/house.svg"
+          alt=""
+          className={`${styles.img} ${styles.house}`}
           />
           :
           <img
-            src="./images/toiletpaper.svg"
-            alt=""
-            className={`${styles.img} ${styles.toiletpaper}`}
+          src="./images/butterfly.svg"
+          alt=""
+          className={`${styles.img} ${styles.butterfly}`}
           />
+        }
 
-          }
 
-          {wpi && wpi["2"].info.Value === "H" ? 
-            <img
-            src="./images/house.svg"
+        {wpi && wpi["3"].info.Value == "F" ?  
+          <img
+            src="./images/chick.svg"
             alt=""
-            className={`${styles.img} ${styles.house}`}
-            />
-            :
-            <img
-            src="./images/butterfly.svg"
-            alt=""
-            className={`${styles.img} ${styles.butterfly}`}
-            />
-          }
-
-
-          {wpi && wpi["3"].info.Value == "F" ?  
-            <img
-              src="./images/chick.svg"
-              alt=""
-              className={`${styles.img} ${styles.chick}`}
-            />
-            :
-            <img
-              src="./images/pokerchip.svg"
-              alt=""
-              className={`${styles.img} ${styles.pokerchip}`}
-            />
-          }
-
-          {wpi && wpi["4"].info.Value === "U" ? 
-            <img
-            src="./images/upchart.svg"
-            alt=""
-            className={`${styles.img} ${styles.upchart}`}
-            />
+            className={`${styles.img} ${styles.chick}`}
+          />
           :
-            <img
-              src="./images/downchart.svg"
-              alt=""
-              className={`${styles.img} ${styles.downchart}`}
-            />
-          }
+          <img
+            src="./images/pokerchip.svg"
+            alt=""
+            className={`${styles.img} ${styles.pokerchip}`}
+          />
+        }
 
-          <div className={styles.timeStamp}>Wallet Birth Date: <br/> {wpi && new Date(wpi["5"].info.Value).toDateString()}</div>
-          
-          
-        </div>
+        {wpi && wpi["4"].info.Value === "U" ? 
+          <img
+          src="./images/upchart.svg"
+          alt=""
+          className={`${styles.img} ${styles.upchart}`}
+          />
+        :
+          <img
+            src="./images/downchart.svg"
+            alt=""
+            className={`${styles.img} ${styles.downchart}`}
+          />
+        }
+
+        <div className={styles.timeStamp}>Wallet Birth Date: <br/> {wpi && new Date(wpi["5"].info.Value).toDateString()}</div>
+        
+        
+      </div>
+        
+        }
+        
       </div>
     </div>
   );
