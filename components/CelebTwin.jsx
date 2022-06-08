061fc5782fed8bafbebb5186e040dbdabc48bac3
Alex <alexcucos1@gmail.com>
2022-06-07 23:22:28 -0400

    Added null handling for celeb twin, updated metrics to match new response schema. fine-tuned mobile styling

--- components/CelebTwin.jsx
+++ components/CelebTwin.jsx
@@ -1,7 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 import React from "react";
-import styles from "../styles/ResultDisplay.module.css";
-import celebStyles from '../styles/CelebTwin.module.css'
+import styles from '../styles/CelebTwin.module.css'
 
 export default function CelebTwin({ wpi }) {
   return (
@@ -9,7 +8,7 @@ export default function CelebTwin({ wpi }) {
       <div className={styles.resultDisplay}>
         
         <div className={styles.celebContainer}>
-          <div className={celebStyles.celebItem}>
+          <div className={styles.celebItem}>
             {/* <img
               className={styles.celebText}
               src="/images/celebText.png"
@@ -39,6 +38,7 @@ export default function CelebTwin({ wpi }) {
 }
 
 const TwitterProfile = ({ twitterObj }) => {
+  
   return (
     <div className={styles.twitterProfileContainer}>
       <div className={styles.twitterInnerProfileContainer}>
@@ -46,17 +46,17 @@ const TwitterProfile = ({ twitterObj }) => {
         <div className={styles.lowerHalfBg}>
           <div className={styles.twitterDetails} style={{overflow: 'hidden'}}>
             <div>
-              <span className={styles.name}>{twitterObj.info.ensName}</span>
-              {twitterObj.info.twitterIsVerified && <img src="/images/verified.png" alt="verfied-tick" />}
+              <span className={styles.name}>{twitterObj && twitterObj.info.ensName}</span>
+              {twitterObj && twitterObj.info.twitterIsVerified && <img src="/images/verified.png" alt="verfied-tick" />}
             </div>
-            <span className={styles.username}>{twitterObj.info.twitterHandle}</span>
-            <span style={{fontSize: '12px', fontWeight: 300}}>{parseInt(twitterObj.info.twitterFollowersCount).toLocaleString('en-US')} followers</span>
-            <p>{twitterObj.info.twitterBio}</p>
+            <span className={styles.username}>{twitterObj && twitterObj.info.twitterHandle}</span>
+            <span style={{fontSize: '12px', fontWeight: 300}}>{twitterObj ? `${parseInt(twitterObj.info.twitterFollowersCount).toLocaleString('en-US')} followers` : 'Looks like you\' a real OG!'} </span>
+            <p>{twitterObj ? twitterObj.info.twitterBio : 'We couldn\'t find a celeb with a similar result and wallet birthday to yours, congrats!'}</p>
           </div>
         </div>
         <img
           className={styles.avatar}
-          src={twitterObj.info.twitterProfileUrl}
+          src={twitterObj ? twitterObj.info.twitterProfileUrl : './images/personplaceholder.png'}
           alt="avatar"
         />
       </div>
--- components/Metrics.jsx
+++ components/Metrics.jsx
@@ -4,12 +4,16 @@ export default function Metrics({wpi}) {
   return (
     <div className={styles.metrics}>
         <h3>My Metrics:</h3>
-        <ul>
-            <li>{wpi && wpi["1"].info.Message}</li>
-            <li>{wpi && wpi["2"].info.Message}</li>
-            <li>{wpi && wpi["3"].info.Message}</li>
-            <li>{wpi && wpi["4"].info.Message}</li>
-        </ul>
+        {wpi && <ul>
+            <li><span className={styles.bigValue}>{ wpi["5"].info.Metrics[0].bigValue}</span> {wpi["5"].info.Metrics[0].smallText}</li>
+            <li><span className={styles.bigValue}>{ wpi["1"].info.Metrics[0].bigValue}</span> {wpi["1"].info.Metrics[0].smallText}</li>
+            <li><span className={styles.bigValue}>{ wpi["1"].info.Metrics[1].bigValue}</span> {wpi["1"].info.Metrics[1].smallText}</li>
+            <li><span className={styles.bigValue}>{ wpi["2"].info.Metrics[0].bigValue}</span> {wpi["2"].info.Metrics[0].smallText}</li>
+            <li><span className={styles.bigValue}>{ wpi["3"].info.Metrics[0].bigValue}</span> {wpi["3"].info.Metrics[0].smallText}</li>
+            <li><span className={styles.bigValue}>{ wpi["3"].info.Metrics[1].bigValue}</span> {wpi["3"].info.Metrics[1].smallText}</li>
+            <li><span className={styles.bigValue}>{ wpi["4"].info.Metrics[0].bigValue}</span> {wpi["4"].info.Metrics[0].smallText}</li>
+            <li><span className={styles.bigValue}>{ wpi["4"].info.Metrics[1].bigValue}</span> {wpi["4"].info.Metrics[1].smallText}</li>
+        </ul>}
     </div>
   )
 }
