b0528cb5706d5d317661c65fd6969b77c4d032a8
Alex <alexcucos1@gmail.com>
2022-06-08 23:43:19 -0400

    changed symbol encoding since not working on mobile

--- components/CelebTwin.jsx
+++ components/CelebTwin.jsx
@@ -16,7 +16,7 @@ export default function CelebTwin({ wpi }) {
             <h3>
               Your Celebrity <br /> Charisma twin is...{" "}
               <div className={styles.celebExplanation}>
-                &#128712;
+                🛈
                 <div className={styles.celebExplanationTooltip}>
                   Calculated by finding the nearest wallet birthday of top ETH
                   Leaderboard wallets with your personality type
